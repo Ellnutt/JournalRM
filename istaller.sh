5c2e85edae789e1159cd71a9c1bffaa6a198f77d
Nathan <114447731+Ellnutt@users.noreply.github.com>
2024-10-13 02:24:41 +0100

    Update istaller.sh

--- istaller.sh
+++ istaller.sh
@@ -2,7 +2,7 @@
 # Installer script to download and set up the clear_journal.sh script
 
 # Define the download URL (this is the link to your GitHub raw file or any hosting platform)
-SCRIPT_URL="https://raw.githubusercontent.com/yourusername/journal-clear-script/main/clear_journal.sh"
+SCRIPT_URL="https://github.com/Ellnutt/JournalRM/blob/master/journal.sh"
 
 # Download the script
 curl -o clear_journal.sh $SCRIPT_URL
